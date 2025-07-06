f43c3f5f0a8a7c164b1b0889034686d450dd765c
NotFoundMax <max.developer.fs@gmail.com>
2025-07-06 18:19:41 -0500

    Segundo commit - carpetas ordenadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 // Configuración de rutas principales
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './componentes/home/home.component';
-import { TablasComponent } from './componentes/tablas/tabla.component';
-import { DirectivesComponent } from './componentes/directives/directives.component';
-import { GalleryComponent } from './componentes/gallery/gallery.component';
-import { FetchingComponent } from './componentes/fetching/fetching.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TablasComponent } from './pages/tablas/tabla.component';
+import { DirectivesComponent } from './pages/directives/directives.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { FetchingComponent } from './pages/fetching/fetching.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'directives', component: DirectivesComponent },
   { path: 'gallery', component: GalleryComponent },
-  { path: 'tables', component: TablasComponent },
+  { path: 'tablas', component: TablasComponent },
   { path: 'fetching', component: FetchingComponent },
   { path: '**', redirectTo: 'home' },
   { path: '', component: HomeComponent }, // ruta por defecto
